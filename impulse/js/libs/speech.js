1ba103aed0396b5eb53399458e9d7f7ae2eccbee
Jonathan Leung <me@jonl.org>
2016-05-15 23:22:55 -0700

    refactor reset so that it works on github

--- impulse/js/libs/speech.js
+++ impulse/js/libs/speech.js
@@ -1,14 +1,47 @@
 function Listener (textareaId) {
     var that = this;
 
-    this.reset = function() {
-        var recognizer = new window.SpeechRecognition();
-        var transcription = document.getElementById(textareaId);
+    window.SpeechRecognition = window.SpeechRecognition ||
+    window.webkitSpeechRecognition ||
+    null;
 
-        // Recogniser doesn't stop listening even if the user pauses
-        recognizer.continuous = true;
+    if (window.SpeechRecognition === null) {
+        alert('speech recognition not supported');
+    }
+    else {
+      var recognizer = new window.SpeechRecognition();
+      var transcription = document.getElementById(textareaId);
 
-        // Start recognising
+      // Recogniser doesn't stop listening even if the user pauses
+      recognizer.continuous = true;
+
+      // Start recognising
+      recognizer.onresult = function(event) {
+          var string = '';
+          var isFinal;
+          transcription.textContent = '';
+
+          for (var i = event.resultIndex; i < event.results.length; i++) {
+              isFinal = event.results[i].isFinal;
+              var result = event.results[i][0].transcript;
+
+              if (isFinal) {
+                  string = result;
+              }
+              else {
+                  string += result;
+              }
+              transcription.textContent = string;
+              if (that.onResult) {
+                  that.onResult(string, isFinal);
+              }
+          }
+      };
+
+      recognizer.interimResults = true;
+      recognizer.start();
+
+      this.reset = function() {
         recognizer.onresult = function(event) {
             var string = '';
             var isFinal;
@@ -29,23 +62,8 @@ function Listener (textareaId) {
                     that.onResult(string, isFinal);
                 }
             }
-
         };
+      };
 
-        recognizer.interimResults = true;
-        recognizer.start();
-
-        console.log('reset');
-    };
-
-    window.SpeechRecognition = window.SpeechRecognition ||
-    window.webkitSpeechRecognition ||
-    null;
-
-    if (window.SpeechRecognition === null) {
-        alert('speech recognition not supported');
-    }
-    else {
-        this.reset();
     }
 }
