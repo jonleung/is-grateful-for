ab4cb2bd91084554e6260388c4e3fa40a324e8d4
Jonathan Leung <me@jonl.org>
2016-05-15 22:52:34 -0700

    add impulse

---
+++ impulse/js/libs/speech.js
@@ -0,0 +1,49 @@
+function Listener (textareaId) {
+    var that = this;
+
+    this.reset = function() {
+        var recognizer = new window.SpeechRecognition();
+        var transcription = document.getElementById(textareaId);
+
+        // Recogniser doesn't stop listening even if the user pauses
+        recognizer.continuous = true;
+
+        // Start recognising
+        recognizer.onresult = function(event) {
+            var string = '';
+            var isFinal;
+            transcription.textContent = '';
+
+            for (var i = event.resultIndex; i < event.results.length; i++) {
+                isFinal = event.results[i].isFinal;
+                var result = event.results[i][0].transcript;
+
+                if (isFinal) {
+                    string = result;
+                }
+                else {
+                    string += result;
+                }
+                transcription.textContent = string;
+                if (that.onResult) {
+                    that.onResult(string, isFinal);
+                }
+            }
+
+        };
+
+        recognizer.interimResults = true;
+        recognizer.start();
+    };
+
+    window.SpeechRecognition = window.SpeechRecognition ||
+    window.webkitSpeechRecognition ||
+    null;
+
+    if (window.SpeechRecognition === null) {
+        alert('speech recognition not supported');
+    }
+    else {
+        this.reset();
+    }
+}
---
+++ impulse/js/main.js
@@ -0,0 +1,53 @@
+var mouseX;
+var mouseY;
+var textArea = null;
+var firstTime = true;
+
+$(document).mousemove(function() {
+    mouseX = event.pageX;
+    mouseY = event.pageY;
+    textArea = createNodeAtCursor(mouseX, mouseY, '');
+});
+
+var listener = new Listener('listener');
+listener.onResult = function(text, isFinal) {
+    if (isFinal === false) {
+        if (textArea === null)  {
+            textArea = createNodeAtCursor(mouseX, mouseY, text);
+        }
+        else {
+            textArea.text(text);
+        }
+    }
+    else {
+        textArea.text(text);
+        textArea = null;
+    }
+
+    if (firstTime) {
+        $('#directions').remove();
+    }
+}
+
+$(document).mousemove(function(){
+    listener.reset();
+    textArea = null;
+});
+
+
+function createNodeAtCursor(x, y, text) {
+    $span = $('<span>').text(text);
+    var $div = $('<div>')
+                    .addClass('node')
+                    .css({
+                        'left': x+'px',
+                        'top': y+'px'
+                    })
+                    .draggable()
+                    .css('position', 'absolute');
+    $div.append($span);
+
+    $(document.body).append($div);
+
+    return $span;
+}
