9193903e4c4ed25a7fb4ac08ede98180e82e5c1a
Jonathan Leung <me@jonl.org>
2016-06-08 15:14:14 -0400

    add initial version of p5_template

---
+++ p5_template/js/main.js
@@ -0,0 +1,10 @@
+function setup() {
+  var cnv = createCanvas(700, 400);
+  cnv.parent('sketch');
+}
+
+function draw() {
+  // Set the background to black and turn off the fill color
+  background(100);
+  noFill();
+}
