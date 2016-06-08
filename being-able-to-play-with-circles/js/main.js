4bae1f6099f7bae095a29896d2e50a0100c12abb
Jonathan Leung <me@jonl.org>
2016-06-08 17:07:16 -0400

    add variable num circles with variable spacing based on mouse

--- being-able-to-play-with-circles/js/main.js
+++ being-able-to-play-with-circles/js/main.js
@@ -1,10 +1,27 @@
+BLACK = 0;
+WHITE = 255;
+
 function setup() {
   var cnv = createCanvas(700, 400);
   cnv.parent('sketch');
+
 }
 
 function draw() {
-  // Set the background to black and turn off the fill color
-  background(100);
-  noFill();
+  var NUM_CIRCLES = 10;
+  NUM_CIRCLES = round(map(mouseX, 0, width, 0, 10));
+
+  clear();
+  fill(WHITE);
+  rect(0, 0, width-1, height-1);
+
+  var H_SPACING = 100;
+  H_SPACING = map(mouseY, 0, width, 0, 500)
+  console.log(H_SPACING);
+
+  fill(BLACK);
+
+  _.times(NUM_CIRCLES, function(i) {
+    ellipse(50 + i*H_SPACING, 100, 50, 50);
+  })
 }
