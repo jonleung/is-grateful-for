525cbb5bc451f94cc72175e10cb3e17071b04566
Jonathan Leung <me@jonl.org>
2016-06-09 16:20:21 -0400

    move generation of all circles to setup

--- being-able-to-play-with-circles/js/main.js
+++ being-able-to-play-with-circles/js/main.js
@@ -1,43 +1,42 @@
-debugger
-BLACK = 0;
-WHITE = 255;
+var NUM_DOTS = 70000;
 
 var dots;
+
+function generateDots() {
+  _.times(NUM_DOTS, function(i) {
+    var x = random(0, width);
+    var y = random (0, height);
+    var r = random(3, 15);
+    // var r = map(round(random(1, 4)), 1, 2, 3, 11);
+    // var r = random(3, 11);
+    var newDot = new Dot(x, y, r);
+
+    var isTouching = false;
+    for(var i=0; i<dots.length; i++) {
+      var oldDot = dots[i];
+
+      if (newDot.isTouching(oldDot)) {
+        isTouching = true;
+        break;
+      }
+    }
+
+    if (!isTouching) {
+      dots.push(newDot);
+    }
+  })
+}
 
 function setup() {
   var cnv = createCanvas(700, 400);
   cnv.parent('sketch');
 
   dots = [];
-}
-
-function draw() {
-  clear();
-
-  var x = random(0, width);
-  var y = random (0, height);
-  var r = random(10, 200);
-
-  var newDot = new Dot(x, y, r);
-
-  console.log(newDot);
-
-  if (dots.length > 0) {
-    dots.forEach(function(oldDot) {
-      if (newDot.isTouching(oldDot)) {
-
-      }
-      else {
-        dots.push(newDot);
-      }
-    });
-  }
-  else {
-    dots.push(newDot);
-  }
-
-
+  generateDots();
   dots.forEach(function(dot) {
     dot.draw();
   })
+}
+
+function draw() {
 }
