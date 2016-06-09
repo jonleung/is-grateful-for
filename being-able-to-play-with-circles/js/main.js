af5d1b15cb3b4bf797316face0d140207fddd613
Jonathan Leung <me@jonl.org>
2016-06-09 16:25:48 -0400

    make drawn dots drawn one by one

--- being-able-to-play-with-circles/js/main.js
+++ being-able-to-play-with-circles/js/main.js
@@ -32,11 +32,34 @@ function setup() {
   cnv.parent('sketch');
 
   dots = [];
-  generateDots();
+  // generateDots();
   dots.forEach(function(dot) {
     dot.draw();
   })
 }
 
 function draw() {
+  debugger
+  var x = random(0, width);
+  var y = random (0, height);
+  var r = random(3, 15);
+  // var r = map(round(random(1, 4)), 1, 2, 3, 11);
+  // var r = random(3, 11);
+  var newDot = new Dot(x, y, r);
+
+  var isTouching = false;
+  for(var i=0; i<dots.length; i++) {
+    var oldDot = dots[i];
+
+    if (newDot.isTouching(oldDot)) {
+      isTouching = true;
+      break;
+    }
+  }
+
+  if (!isTouching) {
+    dots.push(newDot);
+    newDot.draw();
+  }
+
 }
