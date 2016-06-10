c5db56245d570e70b7e8bce65050175fbea8c47b
Jonathan Leung <me@jonl.org>
2016-06-09 22:13:24 -0400

    add vertical slider as well as more sliders

--- vera_molnar/js/p5_helpers.js
+++ vera_molnar/js/p5_helpers.js
@@ -1,3 +1,11 @@
+var drawLabelFunctions = [];
+
+function drawLabels() {
+  drawLabelFunctions.forEach(function(drawLabelFunction) {
+    drawLabelFunction();
+  })
+}
+
 function createLabeledSlider(x, y, label, a, b, defaultValue, width) {
   if (width === undefined) {
     width = 80;
@@ -7,20 +15,23 @@ function createLabeledSlider(x, y, label, a, b, defaultValue, width) {
   slider.position(x, y);
   slider.style('width', width+'px');
 
-  function drawLabel() {
+  slider.drawLabel = function() {
     push();
-    clear();
     textSize(15);
+    fill('black')
     text(slider.value() + ' ' + label, x + width + 10, y + 13);
     pop();
   }
-  drawLabel();
 
-  slider.mouseMoved(function() {
-    if(mouseIsPressed) {
-      drawLabel();
-    }
-  })
+  drawLabelFunctions.push(slider.drawLabel);
+
+  return slider;
+}
+
+function createLabeledVerticalSlider(x, y, label, a, b, defaultValue, width) {
+  var slider = createLabeledSlider(x - width/2, y + width/2, label, a, b, defaultValue, width);
+  slider.style('position', 'absolute');
+  slider.style('-webkit-transform', 'rotate(90deg)');
 
   return slider;
 }
--- vera_molnar/js/sketch.js
+++ vera_molnar/js/sketch.js
@@ -1,32 +1,39 @@
-var NUM_SQ_HORIZONTAL = 6;
-var NUM_SQ_VERTICAL = 6;
-
 var COLOR_PALLETES = [
   ['#D1D5D8', '#3498DB', '#F1C40F', '#E74C3C', '#F1C40F'], //http://flatrowors.net/palette/203-flat-wbuttons
   ['#FE6860', '#FE8A71', '#FE6860', '#D9BBAE', '#FE6860'], // http://flatrowors.net/palette/673-archival-funk
   ['#199EC7', '#40BC86', '#EC555C', '#FCB410'] // http://flatrowors.net/palette/451-exotel
 ];
 
-var repsSlider;
+var BORDER_MARGIN_LEFT = 40;
+var BORDER_MARGIN_TOP = 200;
 
 function setup() {
   var cnv = createCanvas(windowWidth-15, windowHeight);
   cnv.parent('sketch');
   colorPalette = _.sample(COLOR_PALLETES);
 
-  repsSlider = createLabeledSlider(10, 10, 'reps', 0, 255, 1, 80);
+  numSqHorizontalSlider = createLabeledSlider(30, 30, '# sq horiz', 0, 10, 6, 80);
+  numSqVerticalSlider = createLabeledVerticalSlider(30, 30, '# sq vert', 0, 10, 6, 80);
+
+  sqWidthSlider = createLabeledSlider(BORDER_MARGIN_LEFT-2, 150, 'sq width', 0, 200, 60, 200);
+  sqMarginSlider = createLabeledSlider(BORDER_MARGIN_LEFT + 350 , 150, 'sq margin', 0, 100, 30, 100);
+
+
+  repsSlider = createLabeledSlider(200, 30, 'reps', 0, 255, 1, 80);
 }
 
 function draw() {
-  var BORDER_MARGIN_LEFT = 40;
-  var BORDER_MARGIN_TOP = 40;
 
-  var SQ_WIDTH = 60;
-  var SQ_MARGIN = 30;
+  var NUM_SQ_HORIZONTAL = numSqHorizontalSlider.value();
+  var NUM_SQ_VERTICAL = numSqVerticalSlider.value();
+
+  var SQ_WIDTH = sqWidthSlider.value();
+  var SQ_MARGIN = sqMarginSlider.value();
   var SQ_PT_TO_PT_WIDTH = SQ_WIDTH + SQ_MARGIN;
 
   var SQ_OVERLAY_REPS = repsSlider.value();
-  // text(SQ_OVERLAY_REPS, 10 + 90, 32);
+
+  clear();
 
   for(var col=0; col<NUM_SQ_HORIZONTAL; col++) {
     for (var row=0; row<NUM_SQ_VERTICAL; row++) {
@@ -38,4 +45,6 @@ function draw() {
       }
     }
   }
+
+  drawLabels();
 }
